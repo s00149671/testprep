9de9d7c48a978f0dd61bd0f15580108ad1dd6c45
s00149671 <s00149671@mail.itsligo.ie>
2017-12-13 01:44:49 +0000

    test

---
+++ testPreperation/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace testPreperation.Models
+{
+    [Table("Category")]
+
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="CategoryID")]
+
+        public int CategoryId { get; set; }
+        [Display(Name = "CategoryID")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> productsInCategory { get; set; }
+
+
+
+
+
+    }
+}
---
+++ testPreperation/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace testPreperation.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display (Name = "ProductID")]
+        public int ProductId { get; set; }
+
+        [ForeignKey("associatedCategory")]
+        public int CategoryId { get; set; }
+
+        [Display(Name = "ProductID")]
+        public string Description { get; set; }
+
+        [Column(TypeName ="date")]
+        [Display(Name ="Date of last Order")]
+        public DateTime? LastOrderDate { get; set; }
+
+        [Column(TypeName = "date")]
+        [Display(Name = "Date of first stocked")]
+        public DateTime? FirstStockedOn { get; set; }
+
+        [Column(TypeName ="Quantity In Stock")]
+        public int QuantityInStock { get; set; }
+
+        [Column(TypeName ="Unit Price")]
+        public float UnitPrice { get; set; }
+
+        public virtual Category associatedCategory { get; set;
+        }
+    }
+}
